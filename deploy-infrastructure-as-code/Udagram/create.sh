7d641e760ebe447091c3b87940bb2822c8e22c65
Angelo Dias <agdias@gmail.com>
2020-04-17 10:36:22 -0300

    work-in-progress

--- deploy-infrastructure-as-code/Udagram/create.sh
+++ deploy-infrastructure-as-code/Udagram/create.sh
@@ -1,17 +1,5 @@
-#/bin/sh
-
-if [ $# -lt 3 ]; then
-  echo "Usage: create.sh <stack name> <template-file> <parameters-file>"
-  exit 1
-fi
-echo "Updating $1 stack..."
 aws cloudformation create-stack \
                    --stack-name $1 \
                    --template-body file://$2  \
-                   --parameters file://$3  \
-                   --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"  \
-                   --region=us-west-2 
-
-echo "stack $1 updated!"
-exit 0
-
+                    --parameters file://$3  \
+                    --region=us-west-2
